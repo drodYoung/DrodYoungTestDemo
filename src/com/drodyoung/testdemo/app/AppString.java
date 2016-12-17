8a5721b16bd6db9ff159b234ce5b29b9c0395f03
wanghailu <wanghailu@le.com>
2016-12-17 21:18:02 +0800

    For ArrayAdapterTest
    Signed-off-by: wanghailu <wanghailu@le.com>

---
+++ src/com/drodyoung/testdemo/app/AppString.java
@@ -0,0 +1,13 @@
+package com.drodyoung.testdemo.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AppString extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
---
+++ src/com/drodyoung/testdemo/widget/ViewVibration.java
@@ -0,0 +1,45 @@
+package com.drodyoung.testdemo.widget;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.view.Menu;
+import android.view.View;
+
+public class ViewVibration extends ListActivity {
+    public static final String[] TITLES = 
+    {
+            "Henry IV (1)",   
+            "Henry V",
+            "Henry VIII",       
+            "Richard II",
+            "Richard III",
+            "Merchant of Venice",  
+            "Othello",
+            "King Lear"
+    };
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); 
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, TITLES));
+        getListView().setOnItemLongClickListener(new OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view,
+                    int position, long id) {
+                if(position%2==0){
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
